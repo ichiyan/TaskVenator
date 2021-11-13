0325054f8b3530f63eb7d7cd15a03db0dde2f24a
ichiyan <gabriela.balisacan01@gmail.com>
2021-11-13 21:58:53 +0800

    changed route in web.php to wildcard route

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/{path?}', 'welcome');
 
-
-// Route::get('{any}',function(){
-//     return view('welcome');
-// })->where('any', '.*');
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
