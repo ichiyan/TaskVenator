ce0848ff850700582c8ee293661f94b36d930ea9
johndaves6240 <johndaves6240@gmail.com>
2021-11-15 22:10:22 +0800

    change display

---
+++ resources/js/components/pages/shop/AddItemForm.js
@@ -0,0 +1,78 @@
+import { Button, Modal } from 'react-bootstrap';
+import { useState } from "react";
+
+
+function AddItemForm (){
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const[item, setItem]= useState({
+    name: '',
+    description: '',
+    price: '',
+  });
+
+  
+    const inputHandler =(e) =>{
+      setItem({
+        ...item,[e.target.name]:  e.target.value
+      });
+      
+    }
+
+    const potionSubmitHandler = (e) =>{
+      e.preventDefault();
+      const data ={
+        name: item.name,
+        description: item.description,
+        price: item.price
+
+
+      }
+      setPotion({
+        name: '',
+        description: '',
+        price: ''
+       
+      });
+    }
+
+  return (  
+     <div>
+         <div>
+         <Button variant="primary" onClick={handleShow}>
+             Create Item
+        </Button>
+        <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Create Item</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <form onSubmit={potionSubmitHandler}>
+          <div className="form-group">
+            <label>Item Name</label>
+            <input className="form-control" name="name" type="text" onChange={inputHandler} value={item.name}/>
+          </div>
+            <div className="form-group">
+            <label>Description</label>
+            <textarea className="form-control" rows="3" name="desciption" onChange={inputHandler} value={item.description}></textarea>
+            </div>
+            <div className="form-group">
+            <label>Price</label>
+            <input name="price" className="form-control" type="number" onChange={inputHandler} value={item.price}/><br></br>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary mr-auto" onClick={handleClose}>Close</button>
+              <button type="submit" className="btn btn-primary" data-dismiss="modal" onClick={handleClose}>Save</button>
+            </div>
+         </form>
+        </Modal.Body>
+      </Modal>
+         </div>
+      </div>
+    
+  );
+}
+export default AddItemForm;
