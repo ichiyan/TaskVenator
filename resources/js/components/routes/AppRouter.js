c12d01d9593d8860dd450a4278d606c68ed488e8
johndaves6240 <johndaves6240@gmail.com>
2021-11-17 17:10:13 +0800

    auth

--- resources/js/components/routes/AppRouter.js
+++ resources/js/components/routes/AppRouter.js
@@ -1,33 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import axios from 'axios';
-
-import Landing from "../pages/Landing";
-import Auth from "../pages/Auth";
+import Login from "../auth/Login";
+import Register from "../auth/Register";
 import Shop from "../pages/shop/shop";
-import Home from "../pages/Home";
-
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.post['Accept'] = 'application/json';
-axios.defaults.withCredentials = true;
-
-axios.interceptors.request.use(function (config){
-    const token = localStorage.getItem('auth_token');
-    config.headers.Authorization = token ? `Bearer ${token}` : '';
-    return config;
-});
 
 const AppRouter = () => {
     return (
         <div>
             <Router>
                 <Routes>
-                    <Route path='/' exact element={<Landing/>}></Route>
-                    <Route path='/register' element={<Auth page="register"/>}></Route>
-                    <Route path='/login' element={<Auth page="login"/>}></Route>
-                    <Route path='/home' element={<Home/>}></Route>
+                    <Route path='/register' element={<Register/>}></Route>
+                    <Route path='/login' element={<Login/>}></Route>
                     <Route path='/shop' element={<Shop/>}></Route>
                 </Routes>
             </Router>
