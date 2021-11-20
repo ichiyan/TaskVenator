524905a02db5301cf2e703f8d3b9e4bfe4a2d746
johndaves6240 <johndaves6240@gmail.com>
2021-11-20 15:15:38 +0800

    adding images for display

--- resources/js/components/pages/shop/Display.js
+++ resources/js/components/pages/shop/Display.js
@@ -10,75 +10,76 @@ function Display (){
                 <h3>Hp Potion</h3>
             </div>
           <div className="shop-items">
-                <h2>Image</h2>
+                <div className="shop-itemsImage">
+                  <img src="assets/images/health-potion1.png"></img>
+                </div>
+                <div classNmae="shop-itemsInfo">
                 <h6>Large Potion</h6>
                 <p>Heals 100hp</p>
                 <Button>12 <br></br> BUY</Button>
+                </div>
+                
           </div>
           <div className="shop-items">
-                <h2>Image</h2>
-                <h6>Large Potion</h6>
+                <div className="shop-itemsImage">
+                  <img src="assets/images/health-potion2.png"></img>
+                </div>
+                <div classNmae="shop-itemsInfo">
+                <h6>Medium Potion</h6>
                 <p>Heals 70hp</p>
                 <Button>12 <br></br> BUY</Button>
+                </div> 
           </div>
           <div className="shop-items">
-                <h2>Image</h2>
-                <h6>Large Potion</h6>
+                <div className="shop-itemsImage">
+                  <img src="assets/images/health-potion3.png"></img>
+                </div>
+                <div classNmae="shop-itemsInfo">
+                <h6>Small Potion</h6>
                 <p>Heals 50hp</p>
                 <Button>12 <br></br> BUY</Button>
+                </div> 
           </div>
           <div className="shop-categoryName">
                 <h3>Powerup Potion</h3>
             </div>
-          <div className="shop-items">
-                <h2>Image</h2>
+            <div className="shop-items">
+                <div className="shop-itemsImage">
+                  <img src="assets/images/powerup-potion1.png"></img>
+                </div>
+                <div classNmae="shop-itemsInfo">
                 <h6>Large Potion</h6>
-                <p>Add 2 seconds stun</p>
+                <p>Add stuns for 2 seconds</p>
                 <Button>12 <br></br> BUY</Button>
+                </div> 
           </div>
           <div className="shop-items">
-                <h2>Image</h2>
+                <div className="shop-itemsImage">
+                  <img src="assets/images/powerup-potion2.png"></img>
+                </div>
+                <div classNmae="shop-itemsInfo">
                 <h6>Medium Potion</h6>
-                <p>Add 1 second stun</p>
+                <p>Add stuns for 1 seconds</p>
                 <Button>12 <br></br> BUY</Button>
+                </div> 
           </div>
+
           <div className="shop-items">
-                <h2>Image</h2>
+                <div className="shop-itemsImage">
+                  <img src="assets/images/powerup-potion3.png"></img>
+                </div>
+                <div classNmae="shop-itemsInfo">
                 <h6>Small Potion</h6>
-                <p>Add 0.5 second stun</p>
+                <p>Add stuns for 0.5 seconds</p>
                 <Button>12 <br></br> BUY</Button>
-          </div>
-          <div className="shop-items">
-                <h2>Image</h2>
-                <h6>Large Potion</h6>
-                <p>Heals 100hp</p>
-                <Button>12 <br></br> BUY</Button>
-          </div>
-          <div className="shop-items">
-                <h2>Image</h2>
-                <h6>Large Potion</h6>
-                <p>Heals 100hp</p>
-                <Button>12 <br></br> BUY</Button>
+                </div> 
           </div>
          
-        
+    
+       
          
-
-        
-            
         </div>
-    //   <div className="shop-category">
-        //   <h3>Hp Potion</h3>
-        //   <div className="shop-items">
-        //         <h2>test</h2>
-        //         <h2>test</h2>
-        //         <h2>test</h2>
-        //         <h2>test</h2>
-        //   </div>
-
-    //      <h3>Powerup Potion</h3>
-
-    //   </div>
+  
     );
 }
 export default Display;
