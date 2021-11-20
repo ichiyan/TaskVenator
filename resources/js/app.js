8685253dcd83a98f12b0a71b2b2ea7adb066e587
johndaves6240 <johndaves6240@gmail.com>
2021-11-20 14:20:36 +0800

    Revert "Revert "auth""
    This reverts commit e37b7a0d3f7ceb631ef172d0ab31adc459bb38f7.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,14 +5,10 @@
  * building robust, powerful web applications using React + Laravel.
  */
 
-
  require('../../node_modules/bootstrap/dist/css/bootstrap.min.css');
- require('../../public/css/landing_auth.css');
  require('../../public/css/shop.css');
  require('../../node_modules/bootstrap/dist/js/bootstrap.bundle.js');
- require('../../node_modules/jquery/dist/jquery.min.js');
- require('../../public/js/particles.js');
-
+ 
 
  /**
   * Next, we will create a fresh React component instance and attach it to
@@ -21,4 +17,4 @@
   */
 
  require('./components/routes/AppRouter');
-
+ 
