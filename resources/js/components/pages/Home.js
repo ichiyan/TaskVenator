869939c7e178527d922a57b234c81087a88ad043
Riasnner <73977565+Riasnner@users.noreply.github.com>
2022-05-25 08:38:02 +0800

    updated landing page

--- resources/js/components/pages/Home.js
+++ resources/js/components/pages/Home.js
@@ -66,23 +66,63 @@ const Home = () => {
             <div id="landing-pixels"></div>
 
             {/* Details */}
-            <div id="productivity" className="d-flex justify-content-center align-items-center">
-                <h1>Increase your productivity</h1>
-            </div>
-            <div id="details" className="d-flex justify-content-center align-items-center">
-                <div className="container col-md-offset-3">
-                    <img src="/images/levelup.png"/>
-                    <h2>Completing tasks allows you to level up and earn points for the shop</h2>
+            <section id="top" className="d-flex justify-content-center align-items-center" data-aos="zoom-in" data-aos-delay="100">
+                <h1><b>About TaskVenator</b></h1>
+            </section>
+            <section id="details" className="d-flex justify-content-center align-items-center" data-aos="zoom-in" data-aos-delay="100">
+                <div className="container col-6">
+                    <h3>TaskVenator is a productivity application, mainly a task manager, that incorporates gamification to boost productivity. 
+                            The aim is to utilize gamified elements such as in-game avatars, party, quests, rewards, and battles to trigger a gratifying 
+                            sense of accomplishment, enjoyment, creativity, social influence, and engagement with the mind to ultimately increase motivation 
+                            and productivity.</h3>
                 </div>
-                <div className="container col-md-offset-3">
-                    <img src="/images/shop.png"/>
-                    <h2>You can use points to buy items for battles or customize your character's looks and gear</h2>
+            </section>
+
+            {/* Details */}
+            <section id="top" className="d-flex justify-content-center align-items-center" data-aos="zoom-in" data-aos-delay="100">
+                <h1><b>Features</b></h1>
+            </section>
+            <section id="features" className="d-flex justify-content-center align-items-center" data-aos="zoom-in" data-aos-delay="100">    
+                <div className="container col-4">
+                    <img src="/images/task.png" width="45%"/>
+                    <h2><b>ToDo List</b></h2>
+                    <h3>Users can create tasks and complete them to earn currency. It is divided into 3 types: In Progress, Repeating Tasks, and Done. 
+                        If tasks are not completed, a penalty will be given: reducing player health and losing gems.</h3>
+                </div>  
+                <div className="container col-4"> 
+                    <img src="/images/character.png" width="50%"/>    
+                    <h2><b>Character Creation and Customization</b></h2>
+                    <h3>Users will be greeted with the Character Creation when they first register, where they can choose their class. You can earn in-game currency to customize your 
+                        character's looks or increase their stats.</h3>
                 </div>
-                <div className="container col-md-offset-3">
-                    <img src="/images/friends.png"/>
-                    <h2>Complete tasks alone or team up with friends</h2>
+            </section>
+            <section id="features" className="d-flex justify-content-center align-items-center" data-aos="zoom-in" data-aos-delay="100">
+                <div className="container col-4">  
+                    <img src="/images/currency.png" width="40%"/>
+                    <h2><b>In-Game Currency</b></h2>
+                    <h3>This currency can be earned by completing tasks and used to buy items and gear in the shop.</h3>
                 </div>
-            </div>
+                <div className="container col-4">  
+                    <img src="/images/shop.png" width="55%"/>
+                    <h2><b>Shop/Inventory</b></h2>
+                    <h3>This is where players can buy items and gear using their earned currency to buy items and gear to help increase player stats or outfits to customize their 
+                        character's looks.</h3>
+                </div>
+            </section>
+            <section id="features" className="d-flex justify-content-center align-items-center" data-aos="zoom-in" data-aos-delay="100">
+                <div className="container col-4"> 
+                    <img src="/images/friends.png" width="90%"/>
+                    <h2><b>Party</b></h2>
+                    <h3>You can create or join a party with your friends and strangers to complete tasks and battle enemies together.</h3>
+                </div>
+                <div className="container col-4"> 
+                    <img src="/images/battle.png" width="70%"/>
+                    <h2><b>Battles</b></h2>
+                    <h3>Once you join a party, you can engage in battles with your fellow party members. Joining battles allows you to earn more rewards. Attacking in battles are in the 
+                        form of completing tasks. The damage when not completing a task is greater in battle than solo.
+                    </h3>
+                </div>
+            </section>
             <Footer page="home"/>
         </div>
     );
